b470f8838b3d9a9410b1abbcedac367d873b9cef
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2022-09-14 00:13:24 +0300

    Initial Commit

---
+++ MovieKO/MovieKOApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieKOApp.swift
+//  MovieKO
+//
+//  Created by YILDIRIM on 14.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieKOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
