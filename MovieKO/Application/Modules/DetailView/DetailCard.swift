669dcb8795a0dae966cfd1adde7e6592f42e74df
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2022-09-23 23:12:16 +0300

    Create DetailView + DetailCard

---
+++ MovieKO/Application/Modules/DetailView/DetailCard.swift
@@ -0,0 +1,20 @@
+//
+//  DetailCard.swift
+//  MovieKO
+//
+//  Created by YILDIRIM on 23.09.2022.
+//
+
+import SwiftUI
+
+struct DetailCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailCard_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailCard()
+    }
+}
