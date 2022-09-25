a276ed7e6e3ac99882a02d4c91fbb99d1d913cae
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2022-09-26 00:10:55 +0300

    Create Remote Service file and Detail Response file

---
+++ MovieKO/Application/Data/Responses/DetailsResponse/DetailsResponse.swift
@@ -0,0 +1,9 @@
+//
+//  DetailsResponse.swift
+//  MovieKO
+//
+//  Created by YILDIRIM on 26.09.2022.
+//
+
+import Foundation
+//Detail Responses 
---
+++ MovieKO/Application/Network/RemoteServices/DetailsRemoteService.swift
@@ -0,0 +1,22 @@
+//
+//  DetailsRemoteService.swift
+//  MovieKO
+//
+//  Created by YILDIRIM on 26.09.2022.
+//
+
+import Foundation
+//Detail Remote Services
+
+protocol DetailsServiceProtocol {
+    //Remote Func
+}
+
+final class DetailsRemoteService: DetailsServiceProtocol, Requestable {
+    
+    typealias TargetEndPoint = SearchEndPoints
+    
+    
+    
+    //Remote Func Detail
+}
