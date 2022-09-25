f405dfcb0e7763425d3e00cc1b6d876ad2705ec4
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2022-09-26 00:05:08 +0300

    Create and write searched items detail model and repository

---
+++ MovieKO/Application/Data/Models/UIModels/SearchedDetailsUIModel.swift
@@ -0,0 +1,10 @@
+//
+//  SearchedDetailsUIModel.swift
+//  MovieKO
+//
+//  Created by YILDIRIM on 26.09.2022.
+//
+
+import Foundation
+//Searched Details UI MOdel
+
---
+++ MovieKO/Application/Data/Repositories/SearchedDetailsRepository.swift
@@ -0,0 +1,9 @@
+//
+//  SearchedDetailsRepository.swift
+//  MovieKO
+//
+//  Created by YILDIRIM on 26.09.2022.
+//
+
+import Foundation
+//Fetch Searched Item Detail Will be here
